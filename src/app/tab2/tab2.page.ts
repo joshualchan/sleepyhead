79fa89118fd662e48e0dd6ab505299c47c0e2588
18xulisa <lnxu@uci.edu>
2021-02-11 18:48:44 -0800

    changed home page to binding variables

--- src/app/tab2/tab2.page.ts
+++ src/app/tab2/tab2.page.ts
@@ -8,32 +8,47 @@ import { Component } from '@angular/core';
 export class Tab2Page {
 
   constructor() {}  
+  public todaysBedtime:string = "12:00 am"; //default value
+  public tomsWaketime:string = "8:00 am"; //default value
+
   public text:string = 'Sleep';
   private sleepTime:number; 
   private wakeTime:number;
 
+  // SET BEDTIME/WAKETIME ON SCREEN ======================================
+  public setBedtime(Date):void {
+    this.todaysBedtime = Date.toLocaleTimeString();
+  } 
 
-  // returns either sleep or wake time, and total minutes slept(only on wake up click, -1 on sleep click)
-  sleepButton():[Date, number] {
+  public setWaketime(Date):void {
+    this.todaysBedtime = Date.toLocaleTimeString();
+  } 
+
+
+  // BUTTON FUNCTIONS ====================================================
+
+  //updates either sleep or wake time (calls below functions )
+  sleepButton():void {
    // let returnVal = this.text=='Sleep' ? this.sleep() : this.wakeUp(); 
     // console.log(returnVal); 
-    return this.text=='Sleep' ? this.sleep() : this.wakeUp();
+    this.text=='Sleep' ? this.sleep() : this.wakeUp();
   };
 
-  // returns [sleep time as a Date object, -1]
+  // changes sleepTime 
   // updates button text
-  sleep():[Date, number] {
+  sleep():void {
     this.sleepTime = Date.now(); 
     this.text = "I'm Awake"; 
-    return [new Date(this.sleepTime), -1];
+    //return [new Date(this.sleepTime), -1];
   };
 
-  // retunrs [wake time as a Date object, total sleep time in minutes, rounded to nearest whole number]
+  // returns [sleep time as Date object, wake time as a Date object, total sleep time in minutes, rounded to nearest whole number]
   // updates button text 
-  wakeUp():[Date, number] {
+  wakeUp():[Date, Date, number] {
     this.wakeTime = Date.now();
     this.text = "Sleep";
-    return [new Date(this.wakeTime), Math.floor((this.wakeTime-this.sleepTime)/1000/60)]; 
+    return [new Date(this.sleepTime), new Date(this.wakeTime), Math.floor((this.wakeTime-this.sleepTime)/1000/60)]; 
+    // change above line to update db instead of returning the value
   };
 
 }
