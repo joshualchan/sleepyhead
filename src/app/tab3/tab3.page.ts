56308d95bac37a3f4dc42a1703504af386c88080
Joshua Chan <joshualchan@gmail.com>
2021-02-17 10:33:42 -0800

    testing database stuff

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,14 +1,34 @@
 import { Component } from '@angular/core';
 
 import { AuthenticationService } from '../services/authentication.service';
-
+import { DatabaseService } from '../services/database.service';
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
 export class Tab3Page {
+  age;
+  constructor(
+    private authenticationService: AuthenticationService,
+    private databaseService: DatabaseService
+  ) {
+    //this.databaseService.getAge("UEZuYf4qFXUrybJhFZxnDAhjZyB2").then(data => console.log(data));
 
-  constructor(private authenticationService: AuthenticationService) {}
+    
+    this.authenticationService.user$.subscribe(async (user) => {
+      this.age = this.databaseService.getAge(user.uid).then(value => {console.log(value);return value;});
+      console.log(this.age);
+    });
+    
+    console.log(this.age);
+  }
+
+  returnAge() {
+    this.authenticationService.user$.subscribe(async (user) => {
+      console.log(this.databaseService.getAge(user.uid));
+    });
+
+  }
 
 }
