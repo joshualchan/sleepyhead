dc49039fb85f8daeb3aff7ba3d2c9c0cdcd2e8db
Joshua Chan <joshualchan@gmail.com>
2021-02-17 13:33:40 -0800

    completed

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,7 +1,9 @@
 import { Component } from '@angular/core';
+import { Observable, BehaviorSubject } from 'rxjs';
 
 import { AuthenticationService } from '../services/authentication.service';
 import { DatabaseService } from '../services/database.service';
+
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
@@ -9,28 +11,29 @@ import { DatabaseService } from '../services/database.service';
 })
 export class Tab3Page {
   age;
+  goal;
   constructor(
     private authenticationService: AuthenticationService,
     private databaseService: DatabaseService
   ) {
     //this.databaseService.getAge("UEZuYf4qFXUrybJhFZxnDAhjZyB2").then(data => console.log(data));
-
-    this.age =0;
-    this.authenticationService.user$.subscribe(async (user) => {
-      this.databaseService.getUser(user.uid).then(value => {
-        console.log(value.age);
+    //this.age = 0;
+    //this.goal = 0;
+    this.authenticationService.user$.subscribe((currentUser) => {
+      var t = this.databaseService.getUser(currentUser.uid).then(value => {
         this.age = value.age;
-      });
-    });
-    
-    console.log(this.age);
-  }
-  /*
-  returnAge() {
-    this.authenticationService.user$.subscribe(async (user) => {
-      console.log(this.databaseService.getUser(user.uid).age);
+        this.goal = value.goal;
+      })
     });
 
-  }*/
+  }
+  
+  returnAge() {
+    console.log(this.age);
+    return this.age;
+  }
+  returnGoal() {
+    return this.goal;
+  }
 
 }
