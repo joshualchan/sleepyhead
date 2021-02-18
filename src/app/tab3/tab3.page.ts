aff7c67e61f135a82d135f629d6794b38148a95e
Greta Yu <gretayu00@gmail.com>
2021-02-18 15:27:08 -0800

    simplify/modify styling, make calendar time human readable

--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,5 +1,4 @@
 import { Component } from '@angular/core';
-import { Observable, BehaviorSubject } from 'rxjs';
 
 import { AuthenticationService } from '../services/authentication.service';
 import { DatabaseService } from '../services/database.service';
@@ -9,31 +8,33 @@ import { DatabaseService } from '../services/database.service';
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss']
 })
+
 export class Tab3Page {
   age;
   goal;
+
   constructor(
     private authenticationService: AuthenticationService,
     private databaseService: DatabaseService
   ) {
-    //this.databaseService.getAge("UEZuYf4qFXUrybJhFZxnDAhjZyB2").then(data => console.log(data));
-    //this.age = 0;
-    //this.goal = 0;
     this.authenticationService.user$.subscribe((currentUser) => {
-      var t = this.databaseService.getUser(currentUser.uid).then(value => {
+      this.databaseService.getUser(currentUser.uid).then(value => {
         this.age = value.age;
         this.goal = value.goal;
       })
     });
-
   }
   
   returnAge() {
-    console.log(this.age);
     return this.age;
   }
+
   returnGoal() {
     return this.goal;
+  }
+
+  translateDateTime(dateTime) {
+    return new Date(dateTime).toLocaleString();
   }
 
 }
