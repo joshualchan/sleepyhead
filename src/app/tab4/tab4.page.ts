b970762ed142d3050139a494cb8748d73de0c74d
18xulisa <lnxu@uci.edu>
2021-02-24 00:10:24 -0800

    card color changes on click

--- src/app/tab4/tab4.page.ts
+++ src/app/tab4/tab4.page.ts
@@ -7,9 +7,37 @@ import { Component, OnInit } from '@angular/core';
 })
 export class Tab4Page implements OnInit {
 
+  times = {
+    'max': [{'sleep':'12:00am', 'wake': '8:00am', 'chosen':false, 'color':'dark'},
+            {'sleep':'12:00am', 'wake': '8:00am', 'chosen':false, 'color':'dark'},
+            {'sleep':'12:00am', 'wake': '8:00am', 'chosen':false, 'color':'dark'}],
+    'consistent': [{'sleep':'12:00am', 'wake': '8:00am', 'chosen':false, 'color':'dark'},
+                  {'sleep':'12:00am', 'wake': '8:00am', 'chosen':false, 'color':'dark'},
+                  {'sleep':'12:00am', 'wake': '8:00am', 'chosen':false, 'color':'dark'}],
+    'overall': [{'sleep':'12:00am', 'wake': '8:00am', 'chosen':false, 'color':'dark'},
+                {'sleep':'12:00am', 'wake': '8:00am', 'chosen':false, 'color':'dark'},
+                {'sleep':'12:00am', 'wake': '8:00am', 'chosen':false, 'color':'dark'}]
+  }
+
   constructor() { }
 
   ngOnInit() {
+  }
+
+  chooseTime(index:number, category:string) {
+    console.log("clicked");
+
+    Object.keys(this.times).forEach( (category) => {
+      this.times[category].forEach( (option) => {
+        option.chosen = false; 
+        option.color = "dark"; 
+      });
+    });
+
+    this.times[category][index].chosen = true; 
+    this.times[category][index].color = "success";
+    
+    // return or save chosen time to db 
   }
 
 }
